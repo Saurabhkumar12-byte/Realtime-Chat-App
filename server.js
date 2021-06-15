808df55c49556c1901b1207994c9a86ab66d6419
Saurabh <saurabhsingh4778698@gmail.com>
2021-06-15 21:26:55 +0530

    working on localhost

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 http.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT} `)
 })
-// app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
